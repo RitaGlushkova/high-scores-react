2f3b0d3044200c52ce977cfa3848c8c75e572389
Margarita Glushkova <m.glushkova1@gmail.com>
2022-03-29 20:54:30 +0100

    level 4 complete

--- src/HighScoreCard.js
+++ src/HighScoreCard.js
@@ -4,11 +4,17 @@ import PlayerScore from "./PlayerScore";
 const HighScoreCard = (props) => {
   return (
     <div className="card-body" style={{ maxWidth: "20rem" }}>
-      <h5 class="card-title">HIGH SCORES: {props.name}</h5>
+      <h5 className="card-title">HIGH SCORES: {props.name}</h5>
       <table className="table table-striped">
         <tbody>
-          {props.scores.map((player) => {
-            return <PlayerScore n={player.n.toUpperCase()} s={player.s} />;
+          {props.scores.map((player, index) => {
+            return (
+              <PlayerScore
+                n={player.n.toUpperCase()}
+                s={player.s}
+                key={index}
+              />
+            );
           })}
         </tbody>
       </table>
--- src/HighScoreTable.js
+++ src/HighScoreTable.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import HighScoreCard from "./HighScoreCard";
 import Scores from "./scores";
 
 const HighScoreTable = (props) => {
-  props = Scores;
+  props = Scores.sort((a, b) =>
+    a.name > b.name ? 1 : b.name > a.name ? -1 : 0
+  );
+  const [sorted, setSorted] = useState(props);
+  // const [sortDirection, setSortDirection] = useState(1);
+
+  const descending = () => {
+    const newSorted = [...sorted].map((el) => ({
+      ...el,
+      scores: el.scores.sort((a, b) => (a.s > b.s ? 1 : b.s > a.s ? -1 : 0)),
+    }));
+    setSorted(newSorted);
+  };
+
+  const ascending = () => {
+    const newSorted = [...sorted].map((el) => ({
+      ...el,
+      scores: el.scores.sort((a, b) => (a.s > b.s ? -1 : b.s > a.s ? 1 : 0)),
+    }));
+    setSorted(newSorted);
+  };
+
   return (
-    <div class="card bg-light mb-3" style= {{maxWidth: "18rem"}}>
-      <div class="card-header">High Scores per Country</div>
-      {props.map((prop) => {
-        return <HighScoreCard name={prop.name} scores={prop.scores} />;
+    <div className="card bg-light mb-3" style={{ maxWidth: "18rem" }}>
+      <div className="card-header">
+        High Scores per Country
+        <button onClick={ascending}>Highest score</button>
+        <button onClick={descending}>Lowest score</button>
+      </div>
+      {props.map((prop, index) => {
+        return (
+          <HighScoreCard key={index} name={prop.name} scores={prop.scores} />
+        );
       })}
     </div>
   );
